18d717394af8383a3767c94b0966dcf3a0be0fb1
AbhayMishra <grabhaymishra@gmail.com>
2024-10-23 17:13:37 +0530

    FIXED: bugs

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Proxy error');
 });
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
