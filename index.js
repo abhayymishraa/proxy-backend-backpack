81bb91e9170d88623a416e5d8cbca82793176d73
AbhayMishra <grabhaymishra@gmail.com>
2024-10-23 22:33:50 +0530

    FIXED: bugs

--- index.js
+++ index.js
@@ -32,38 +32,27 @@ app.options('*', (req, res) => {
 });
 
 // Function to mock browser headers
-const setBrowserHeaders = (proxyReq, req) => {
-    proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
-    proxyReq.setHeader('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8');
-    proxyReq.setHeader('Accept-Language', 'en-US,en;q=0.9');
-    proxyReq.setHeader('Accept-Encoding', 'gzip, deflate, br');
-    proxyReq.setHeader('Referer', allowedOrigin);
-    proxyReq.setHeader('Origin', allowedOrigin);
-    proxyReq.setHeader('Connection', 'keep-alive');
-    proxyReq.setHeader('Upgrade-Insecure-Requests', '1');
-    proxyReq.setHeader('Cache-Control', 'max-age=0');
-    proxyReq.setHeader('DNT', '1'); // Do Not Track
-};
 
-// Proxy middleware
 app.use('/', createProxyMiddleware({
     target: targetUrl,
     changeOrigin: true,
     onProxyReq: (proxyReq, req, res) => {
         console.log(`Proxying request to: ${targetUrl}${req.url}`);
 
-        // Set headers to simulate a browser request
-        setBrowserHeaders(proxyReq, req);
-
-        // Forward cookies if they exist (simulating a real browser session)
-        if (req.headers.cookie) {
-            proxyReq.setHeader('Cookie', req.headers.cookie);
-        }
+        // Set the headers to mimic a request from backpack.exchange
+        proxyReq.setHeader('Origin', 'https://backpack.exchange');
+        proxyReq.setHeader('sec-fetch-site', 'same-site');
+        proxyReq.setHeader('sec-fetch-mode', 'cors');
+        proxyReq.setHeader('sec-fetch-dest', 'empty');
+        proxyReq.setHeader('sec-ch-ua', '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"');
+        proxyReq.setHeader('sec-ch-ua-mobile', '?0');
+        proxyReq.setHeader('sec-ch-ua-platform', '"Linux"');
     },
     onProxyRes: (proxyRes, req, res) => {
         console.log(`Response received with status: ${proxyRes.statusCode}`);
     }
 }));
+
 
 // Error handling
 app.use((err, req, res, next) => {
