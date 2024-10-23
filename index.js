70ffd950a7b9ff491974b6579af0c2b0a3b8f239
AbhayMishra <grabhaymishra@gmail.com>
2024-10-23 16:28:07 +0530

    FIXED: bugs

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const targetUrl = 'https://api.backpack.exchange';
 
+
+const allowedOrigin = 'https://next-trade-frontend.vercel.app'; 
+
 // Handle CORS
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Expose-Headers', 'Content-Length, Content-Range');
+    const origin = req.headers.origin;
+    if(origin  == allowedOrigin){
+
+        res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Expose-Headers', 'Content-Length, Content-Range');
+    }
+
     next();
 });
 
