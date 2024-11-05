6a1e4229bba6731703b105c9af3c0d15eaee3153
Abhay <grabhaymishra@gmail.com>
2024-11-05 17:41:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ const allowedOrigin = 'https://next-trade-frontend.vercel.app';
 
 // Handle CORS
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*");
+    const origin = req.headers.origin;
+    if (origin === allowedOrigin) {
+        res.header('Access-Control-Allow-Origin', "*");
+    }
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
 
+// Enable CORS
 
 // Handle OPTIONS preflight requests
 app.options('*', (req, res) => {
