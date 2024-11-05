eb4ee4eda4b16c0246094e9c3cddab1deaabe4bd
AbhayMishra <grabhaymishra@gmail.com>
2024-11-05 12:18:57 +0530

    FIXED: bugs

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ const allowedOrigin = 'https://next-trade-frontend.vercel.app';
 
 // Handle CORS
 app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (origin === allowedOrigin) {
-        res.header('Access-Control-Allow-Origin', origin);
-    }
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
